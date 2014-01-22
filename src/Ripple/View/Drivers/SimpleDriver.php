420120efc3b7389e0aa7008703b0bfec552a0212
James <james.a.tracy@gmail.com>
2014-01-21 21:09:33 -0500

    SimpleDriver for php views

---
+++ src/Ripple/View/Drivers/SimpleDriver.php
@@ -0,0 +1,38 @@
+<?php
+namespace Ripple\View\Drivers;
+
+
+/**
+ * A simple php view driver. The view is a valid php file.
+ *
+ * @since 0.1.0
+ * @author	James Tracy <james.a.tracy@gmail.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ */
+class SimpleDriver
+{
+    /** 
+     * Render the view and return the output string.
+     * @since 0.1.0
+     * @param string $path
+     * @param array $params View parameters
+     * @return string
+     */
+    public function render($path, $params = array())
+    {
+        extract($params);
+        
+        ob_start();
+        
+        try {
+            include $path;
+        } catch(\Exception $e) {
+            // throw away the output
+            ob_get_clean();
+            throw $e;
+        }
+        
+        return ob_get_clean();
+    }
+};
+?>
